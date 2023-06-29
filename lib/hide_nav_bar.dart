33c54c57f6f5d770498435175a9a1c60be3b6b05
Adabe Desmond <adabedesmond@gmail.com>
2023-06-29 15:06:44 +0000

    comnitting new file

---
+++ lib/hide_nav_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ScrollToHideNavBar extends StatefulWidget {
+   final ScrollController controller;
+  final Widget child;
+  final Duration duration;
+  const ScrollToHideNavBar({
+    Key? key,
+    required this.child,
+    required this.controller,
+    this.duration = const Duration(seconds:2),
+  }) : super(key: key);
+ 
+
+  @override
+  State<ScrollToHideNavBar> createState() => _ScrollToHideNavBarState();
+}
+
+class _ScrollToHideNavBarState extends State<ScrollToHideNavBar> {
+   bool _isVisible = true;
+
+   @override
+   void initState() {
+    super.initState();
+    widget.controller.addListener(listen);
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(listen);
+    super.dispose();
+  }
+
+  listen(){
+    final direction = widget.controller.position.userScrollDirection;
+    if(direction == ScrollDirection.reverse){
+      show();
+    }
+    else if(direction == ScrollDirection.forward){
+      hide();
+    }
+  }
+   
+  //  show bottom nav bar
+  void show(){
+    if(!_isVisible) {
+      setState(() => _isVisible = false);
+    }
+  }
+
+  // hide bottom nav bar
+  void hide(){
+    if (_isVisible) {
+      setState(() => _isVisible = true);
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: widget.duration,
+      height:_isVisible? kBottomNavigationBarHeight : 0,
+      child: Wrap(children: [widget.child])
+  );
+  }
+}
